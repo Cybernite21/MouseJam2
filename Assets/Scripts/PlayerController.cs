37a20dd2535105a083cacbf8c320e10eedf60421
Cybernite21 <69601292+Cybernite21@users.noreply.github.com>
2023-03-15 16:37:59 -0500

    Added movement
    Added movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10f;
     Vector3 mousePos;
 
     // Start is called before the first frame update
@@ -16,13 +17,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mousePos.x = transform.position.x + 2;
         transform.up = (mousePos - transform.position).normalized;
+        transform.position += transform.up * speed * Time.deltaTime;
     }
 
     public void OnLook(InputValue input)
     {
         mousePos = Camera.main.ScreenToWorldPoint(input.Get<Vector2>());
         mousePos.z = transform.position.z;
+        mousePos.x = transform.position.x + 2;
         print(mousePos);
     }
 }
