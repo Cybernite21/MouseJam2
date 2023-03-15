906488abc5009d95c4ffc6f2e77f03882eb32cbe
Cybernite21 <69601292+Cybernite21@users.noreply.github.com>
2023-03-15 16:12:46 -0500

    Added a base Player Controller
    Added a base Player Controller, just looks at mouse for now

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector3 mousePos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mousePos = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.up = (mousePos - transform.position).normalized;
+    }
+
+    public void OnLook(InputValue input)
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(input.Get<Vector2>());
+        mousePos.z = transform.position.z;
+        print(mousePos);
     }
 }
